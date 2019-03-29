85e8ac4baf150c29b33c1b736e6183ea4a084f6f
FEgor2004 <fegor2004@yandex.ru>
2019-03-29 22:03:02 +0300

    You can make a blank for a olympic coding
    1. Copy input file`s name to a clipboard
    2. Press Ctrl+Shift+U to make a blank.

---
+++ olymp.py
@@ -0,0 +1,11 @@
+import sublime
+import sublime_plugin
+
+
+class OlympCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		#self.view.set_syntax_file('C++');
+		clipboard=sublime.get_clipboard();	
+		self.view.insert(edit, 0, '#include <bits/stdc++.h>\nusing namespace std;\nint main() {\n\tfreopen("' + clipboard + '.in", "r", stdin);\n\tfreopen("' + clipboard + '.out", "w", stdout);\n\treturn 0;\n}')
+		self.view.set_syntax_file('Packages/C++/C++.sublime-syntax')
+		self.sublime.Window.new_file()
