ccb0d660e484f5eb75211e39eb9bb9b1993e6c42
Egor <fegor2004@gmail.com>
2019-08-10 11:50:14 +0300

    Update olymp.py

--- olymp.py
+++ olymp.py
@@ -6,6 +6,6 @@ class OlympCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		#self.view.set_syntax_file('C++');
 		clipboard=sublime.get_clipboard();	
-		self.view.insert(edit, 0, '#include <bits/stdc++.h>\n#define DEBUG\n#define watch(x) cout << (#x) << " is " << (x) << endl\nusing namespace std;\nint main() {\n\t// ios_base::sync_with_stdio(0);\n\t// cin.tie(NULL);\n\t#ifdef DEBUG\n\t\tfreopen("input.txt", "r", stdin);\n\t\tfreopen("output.txt", "w", stdout);\n\t#else\n\t\tfreopen("' + clipboard + '.in", "r", stdin);\n\t\tfreopen("' + clipboard + '.out", "w", stdout);\n\t#endif\n\treturn 0;\n}')
+		self.view.insert(edit, 0, '#include <bits/stdc++.h>\n#define DEBUG\nusing namespace std;\n#pragma GCC target ("avx2")\n#pragma GCC optimization ("O3")\n#pragma GCC optimization ("unroll-loops")\nint main() {\n\t// ios_base::sync_with_stdio(0);\n\t// cin.tie(NULL);\n\t#ifdef DEBUG\n\tfreopen("input.txt", "r", stdin);\n\tfreopen("output.txt", "w", stdout);\n\t#else\tfreopen("' + clipboard + '.in", "r", stdin);\n\tfreopen("' + clipboard + '.out", "w", stdout);\n\t#endif\n\treturn 0;\n}')
 		self.view.set_syntax_file('Packages/C++/C++.sublime-syntax')
 		self.sublime.Window.new_file()
